61987778a666c9c62c2400e576c5d9c2ac7db841
Svetlin Yotov <svetlin.yotov@gmail.com>
2019-03-12 20:07:04 +0200

    Added buttons on main login screen , added some google and facebook api tokens

--- app/src/main/java/bg/tusofia/sem6/pmu/myapplication/MainActivity.java
+++ app/src/main/java/bg/tusofia/sem6/pmu/myapplication/MainActivity.java
@@ -1,13 +1,14 @@
 package bg.tusofia.sem6.pmu.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
