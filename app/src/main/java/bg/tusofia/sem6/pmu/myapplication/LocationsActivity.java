5d28a90befad4bbbb59137e0e34f009de5e485c3
primu97 <primu@abv.bg>
2019-03-24 14:36:10 +0200

    Bg flag, background music, start game appear when close, pop up for info prototype

--- app/src/main/java/bg/tusofia/sem6/pmu/myapplication/LocationsActivity.java
+++ app/src/main/java/bg/tusofia/sem6/pmu/myapplication/LocationsActivity.java
@@ -3,6 +3,7 @@ package bg.tusofia.sem6.pmu.myapplication;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.location.Criteria;
@@ -15,9 +16,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.util.Log;
 import android.view.View;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.PopupWindow;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -46,14 +52,20 @@ import bg.tusofia.sem6.pmu.myapplication.ServerRequest.RequestBuilder;
 import bg.tusofia.sem6.pmu.myapplication.ServerRequest.URL;
 import bg.tusofia.sem6.pmu.myapplication.Utils.AlertDialog;
 import bg.tusofia.sem6.pmu.myapplication.Utils.StoredData;
+import bg.tusofia.sem6.pmu.myapplication.services.playAudioService;
 
 import static bg.tusofia.sem6.pmu.myapplication.Utils.PermissionCheck.LOCATION_PERMISSION_REQUEST_CODE;
 
 public class LocationsActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private Context mContext;
     private GoogleMap mMap;
     private SupportMapFragment mapFragment;
+    private PopupWindow mPopupWindow;
+    private RelativeLayout mRelativeLayout;
     private Request serverRequest;
     private Location currentLocation = null;
+    private ImageButton startGame;
+    private TextView textStartGame;
     private TreeSet<LatLng> markers = new TreeSet<>((a, b) -> {
         if (currentLocation != null) {
             Location locationA = new Location("point A");
@@ -75,6 +87,11 @@ public class LocationsActivity extends AppCompatActivity implements OnMapReadyCa
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_locations);
+        mRelativeLayout = findViewById(R.id.locations);
+
+        mContext = getApplicationContext();
+
+        startService(new Intent(LocationsActivity.this, playAudioService.class));
 
         serverRequest = new Request(this);
 
@@ -85,10 +102,15 @@ public class LocationsActivity extends AppCompatActivity implements OnMapReadyCa
 
 
         Button buttonLogOut = findViewById(R.id.buttonLogOut);
+        Button mute = findViewById(R.id.buttonMute);
+        startGame = findViewById(R.id.buttonStartGame);
+        textStartGame = findViewById(R.id.textStartGame);
+
 
         ((TextView) findViewById(R.id.textView)).setText("Email: " + StoredData.getString(this, StoredData.LOGGED_USER_EMAIL));
 
         buttonLogOut.setOnClickListener((v) -> Auth.logOut(this));
+        mute.setOnClickListener((v) -> stopService(new Intent(LocationsActivity.this, playAudioService.class)));
     }
 
     @Override
@@ -117,10 +139,31 @@ public class LocationsActivity extends AppCompatActivity implements OnMapReadyCa
             zoomControlsParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
             zoomControlsParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
             zoomControlsParams.setMargins(0, 0, 0, 220);
+
         }
 
+
         mMap.setOnMarkerClickListener(marker -> {
-            //TODO
+            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(LAYOUT_INFLATER_SERVICE);
+            View popup = inflater.inflate(R.layout.marker_info_popup, null);
+
+            mPopupWindow = new PopupWindow(
+                    popup,
+                    LayoutParams.WRAP_CONTENT,
+                    LayoutParams.WRAP_CONTENT
+            );
+            mPopupWindow.setElevation(5.0f);
+            Button closeButton = popup.findViewById(R.id.dismiss);
+
+            closeButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mPopupWindow.dismiss();
+                }
+            });
+            mPopupWindow.showAtLocation(mRelativeLayout, Gravity.CENTER, 0, 0);
+
+
             return true;
         });
     }
@@ -128,6 +171,7 @@ public class LocationsActivity extends AppCompatActivity implements OnMapReadyCa
     @Override
     protected void onStop() {
         super.onStop();
+        stopService(new Intent(LocationsActivity.this, playAudioService.class));
         serverRequest.stop();
     }
 
@@ -155,7 +199,7 @@ public class LocationsActivity extends AppCompatActivity implements OnMapReadyCa
                             if (locations != null) {
                                 int height = 200;
                                 int width = 200;
-                                Bitmap b = ((BitmapDrawable) getResources().getDrawable(R.drawable.marker_icon, this.getTheme())).getBitmap();
+                                Bitmap b = ((BitmapDrawable) getResources().getDrawable(R.drawable.bg_flag, this.getTheme())).getBitmap();
                                 Bitmap smallMarker = Bitmap.createScaledBitmap(b, width, height, false);
 
                                 LatLngBounds.Builder builder = new LatLngBounds.Builder();
@@ -174,6 +218,7 @@ public class LocationsActivity extends AppCompatActivity implements OnMapReadyCa
 
                                 LatLngBounds bounds = builder.build();
 
+
                                 if (!mMap.isMyLocationEnabled() || currentLocation == null) {
                                     if (locations.size() == 1) {
                                         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(bounds.getCenter(), 15));
@@ -185,6 +230,16 @@ public class LocationsActivity extends AppCompatActivity implements OnMapReadyCa
                                     nearestMarkerAndLocationBounds.include(markers.first()); // Get the nearest marker form the sorted set
                                     nearestMarkerAndLocationBounds.include(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()));
                                     mMap.animateCamera(CameraUpdateFactory.newLatLngBounds(nearestMarkerAndLocationBounds.build(), 150));
+                                }
+
+                                Location a = new Location("KUR");
+                                LatLng c = markers.first();
+                                a.setLatitude(c.latitude);
+                                a.setLongitude(c.longitude);
+                                if (currentLocation.distanceTo(a) < 10000) {
+                                    startGame.setVisibility(View.VISIBLE);
+                                    textStartGame.setVisibility(View.VISIBLE);
+
                                 }
                             }
                         })
